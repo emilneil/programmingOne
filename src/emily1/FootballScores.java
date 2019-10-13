315e0b03d56bf74220bdcc7638c4a9bff812d70b
emily c <55987488+emilneil@users.noreply.github.com>
2019-10-13 12:46:54 +0100

    hello no file

---
+++ src/emily1/FootballScores.java
@@ -0,0 +1,73 @@
+package emily1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FootballScores {
+    public static void main(String[] args){
+        System.out.println("hello");
+        int valid = 0;
+        int invalid=0;
+        try{
+            FileReader fileRead = new FileReader("resources/fbScores.txt");
+            BufferedReader buff = new BufferedReader(fileRead);
+            Scanner fileScan = new Scanner(buff);
+            while (fileScan.hasNextLine()){
+                String lines = fileScan.nextLine();
+                String[] infos = lines.split(":");
+
+                if (infos.length == 4){
+                    infos[0]= infos[0].trim();
+                    infos[1]= infos[1].trim();
+                    infos[2]= infos[2].trim();
+                    infos[3]= infos[3].trim();
+
+                    int broken = 0;
+                    for(String info : infos){
+                        if(info.equalsIgnoreCase("")) {
+                            broken += 1;
+                        }
+                    }
+                    if(broken == 0) {
+                        try {
+
+                            int numberOne = Integer.parseInt(infos[2]);
+                            int numberTwo = Integer.parseInt(infos[3]);
+
+                            String game = infos[0] + " " + infos[2] + " Vs " + infos[1] + " " + infos[3];
+                            System.out.println(game);
+                            if (numberOne > numberTwo) {
+                                String gameWinner = infos[0] + " won";
+                                System.out.println(gameWinner);
+                            } else if (numberOne < numberTwo) {
+                                String gameWinner = infos[1] + "won";
+                                System.out.println(gameWinner);
+                            } else {
+                                String gameWinner = "it was a draw";
+                                System.out.println(gameWinner);
+                            }
+                            valid += 1;
+                        } catch (NumberFormatException e) {
+                            invalid += 1;
+                        }
+                    }
+                    else{
+                        invalid+=1;
+                    }
+                }
+                else{
+                    invalid+=1;
+                }
+            }
+        }
+        catch(IOException e){
+            System.out.println("no file");
+        }
+        }
+
+
+}
